2e34bbf9e975bacedd0f7f54ebbf5664f3b8b4fb
Jesse Millar <jesse.millar@microsoft.com>
2021-07-30 14:00:30 -0500

    Close enough

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 const baseURL = "https://chromasdk.io:54236"
@@ -22,8 +25,15 @@ type appCreationRequest struct {
 }
 
 type appCreationResponse struct {
-	Sessionid int    `json:"sessionid"`
+	SessionID int    `json:"sessionid"`
 	URI       string `json:"uri"`
+}
+
+type effectCreateRequest struct {
+	Effect string `json:"effect"`
+	Param  struct {
+		Color int `json:"color"`
+	} `json:"param"`
 }
 
 type effectApplyRequest struct {
@@ -31,28 +41,24 @@ type effectApplyRequest struct {
 }
 
 func main() {
-	fmt.Println("vim-go")
+	createApp()
 
-	// TODO Create app
-	// TODO Make a Goroutine to ping health
+	go pingHeartbeat()
+
+	fmt.Println(sessionID)
+
 	// TODO Create lighting effect
 	// TODO Apply lighting effect
 	// TODO Test latency/request limits
 }
 
-func makeRequest(url string, payload string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		// TODO Handle errors instead of panic
-		log.Fatalln(err)
-	}
-}
-
-// Need to ping more often than every 15 seconds
 func pingHeartbeat() {
-	resp, err := http.Get(getSessionURL() + "/heartbeat")
-	if err != nil {
-		log.Fatalln(err)
+	// TODO Make a way to end this
+	for range time.Tick(time.Second * 1) {
+		resp, err := http.Put(getSessionURL() + "/heartbeat")
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 }
 
@@ -61,9 +67,38 @@ func getSessionURL() string {
 }
 
 func createApp() {
-	makeRequest(getSessionURL()+"/razer/chromasdk", nil)
+	app := appCreationRequest{}
+
+	resp, err := http.Post(getSessionURL()+"/razer/chromasdk", app)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var data appCreationResponse
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	sessionID = data.SessionID
 }
 
 func createEffect() {
-	makeRequest(getSessionURL()+"/chromalink", nil)
+	resp, err := http.Post(getSessionURL()+"/chromalink", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func applyEffect() {
+	// TODO Handle Put
+	resp, err := http.Put(getSessionURL()+"/chromalink", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
