45d3fad829dfcc7a1e655dbff23c1507080ed723
Jesse Millar <jesse.millar@microsoft.com>
2021-09-15 12:56:35 -0500

    Removing dupe hashes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -39,10 +39,10 @@ func flashColor(color string, flashCount string, flashDuration string) {
 	} else {
 		for i := 0; i < flashCountInt; i++ {
 			fmt.Println("Setting color to " + color)
-			razer.SetColor("#" + color)
+			razer.SetColor(color)
 			time.Sleep(time.Duration(flashDurationFloat) * time.Second)
 			fmt.Println("Setting color to black")
-			razer.SetColor("#000000")
+			razer.SetColor("000000")
 			time.Sleep(time.Duration(flashDurationFloat) * time.Second)
 		}
 	}
