6dc047b2fbbdcfe2cf60f29fbe0136690899c27c
Jesse Millar <jesse.millar@microsoft.com>
2021-09-15 11:48:04 -0500

    Chipping away compiler errors

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/icza/gox/imagex/colorx"
+	"github.com/jessemillar/razer-chroma-go-wrapper/internal/utils"
+	"github.com/jessemillar/razer-chroma-go-wrapper/pkg/razer"
 	"github.com/labstack/echo/v4"
 )
 
 func ChangeColor(c echo.Context) error {
 	color := c.Param("color")
 	parsedColor, _ := colorx.ParseHexColor("#" + color)
-	createAndApplyEffect(convertColor(int(parsedColor.R), int(parsedColor.G), int(parsedColor.B)))
+	razer.CreateAndApplyEffect(utils.ConvertColor(int(parsedColor.R), int(parsedColor.G), int(parsedColor.B)))
 	return c.String(http.StatusOK, color)
 }
