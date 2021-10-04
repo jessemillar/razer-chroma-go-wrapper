7362194aa170288119be0ad65ea70cad9fb4a459
Jesse Millar <jesse.millar@microsoft.com>
2021-10-04 12:12:20 -0500

    Working custom colors

--- main_test.go
+++ main_test.go
@@ -4,14 +4,21 @@ import (
 	"testing"
 
 	"github.com/jessemillar/razer-chroma-http-wrapper/internal/utils"
-	"github.com/spf13/viper"
 )
 
 func TestCustomColor(t *testing.T) {
 	utils.ReadConfigFile()
-	t.Log(viper.GetString("default_color"))
 	color := utils.TranslateCustomColor("pink")
 	if color != "#f3b8b6" {
+		t.Log(color)
+		t.Fail()
+	}
+}
+
+func TestCustomColorAlias(t *testing.T) {
+	utils.ReadConfigFile()
+	color := utils.TranslateCustomColor("success")
+	if color != "#4ECDC4" {
 		t.Log(color)
 		t.Fail()
 	}
